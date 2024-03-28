5dd4e16acf2d85d7c5c12b6cf4e43465fb500edc
Ephyrian <qwertymiky@hotmail.it>
2024-03-28 14:31:41 +0100

    9. Deposit tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -2,18 +2,46 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "./Token.sol";             /* we import also the Token.sol to use it for these contract's functions and to use that functions for this contract */
+
 
 contract Exchange {
 	
 	address public feeAccount;
 	uint256 public feePercent;
 
+		mapping(address => mapping(address => uint256))	public tokens;						/* this nested mapping tracks all users and their deposits, the first address is the user address, the second is the token address and then its value */
+		
+		event Deposit(address token, address user, uint256 amount, uint256 balance);
 
 	constructor(
 		address _feeAccount,
 		uint256 _feePercent
 		) {
-		feeAccount = _feeAccount;
-		feePercent = _feePercent;
+		feeAccount = _feeAccount;   /* this is the address where the fees got paid */
+		feePercent = _feePercent;   /* this is the percentage of the fee that the Exchange takes */
 	}
+
+	//deposit tokens
+	function depositToken(address _token, uint256 _amount) public {
+		//transfer tokens to exchange
+		require(Token(_token).transferFrom(msg.sender, address(this), _amount));              /* we call 'Token' that is the name of the smart contract imported before in the import list, then we assign it to the '_token' address to use it in this smart contract. we chose to use the 'transferFrom(from,to,value)' function from the Token.sol smart contract to transfer tokens from a wallet to the exchange smart contract. 'address(this)' means the address of this smart contract */
+
+		//update user balance
+		tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
+
+		//emit an event
+		emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+	}
+
+
+	//check balance
+	function balanceOf(address _token, address _user)     /* this is an alternative to show what a mapping result could be returned */
+		public 
+		view
+		returns(uint256)
+		{
+			return tokens[_token][_user];
+		}
+
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -9,17 +9,24 @@ describe('Exchange', () => {
 
 	let deployer, feeAccount, exchange
 
-	const feePercent = 10
+	const feePercent = 10                                                  /* this is the value choosen for the Exchange fees as a global var */
 
 	beforeEach(async () => {
 
+		const Exchange = await ethers.getContractFactory('Exchange')
+		const Token = await ethers.getContractFactory('Token')             /* we import Token contract also in the test */
+
+		token1 = await Token.deploy('Dapp University', 'DAPP', '1000000')			   /* we could have more than a token in the exchange so this is token1 */
+
 		accounts = await ethers.getSigners()
 		deployer = accounts[0]
-		feeAccount = accounts[1]
+		feeAccount = accounts[1]										   /* this is the address where the fees got paid */
+		user1 = accounts[2]												   /* we could have more than a user also */
 
+		let transaction = await token1.connect(deployer).transfer(user1.address, tokens(100)) /* we assign 100 tokens to the user1 at deployment time because so it's possible to continue the depositing test */
+		await transaction.wait()
 
-		const Exchange = await ethers.getContractFactory('Exchange')
-		exchange = await Exchange.deploy(feeAccount.address, feePercent)
+		exchange = await Exchange.deploy(feeAccount.address, feePercent) /* the smart contract is deployed... */
 
 	})
 
@@ -27,11 +34,58 @@ describe('Exchange', () => {
 	describe('Deployment', () => {
 
 		it('tracks the fee account', async () => {
-			expect(await exchange.feeAccount()).to.equal(feeAccount.address)
+			expect(await exchange.feeAccount()).to.equal(feeAccount.address)     /* checking if the fee account is working */
 		})
 
 		it('tracks the fee percent', async () => {
-			expect(await exchange.feePercent()).to.equal(feePercent)
+			expect(await exchange.feePercent()).to.equal(feePercent)            /* checking if the percentage of fees is correct */
 		})
 	})
+
+	describe('Depositing tokens', () => {
+		
+		let transaction, result
+		let amount = tokens(10)                                  /* 10 tokens are assigned to be used in the transaction test */
+
+		
+
+		describe('Success', () => {
+
+			beforeEach(async () => {
+
+		//approve token
+		transaction = await token1.connect(user1).approve(exchange.address, amount)
+		result = await transaction.wait()         /* wait for the finish of the transaction */
+
+		//deposit tokens
+		transaction = await exchange.connect(user1).depositToken(token1.address, amount)
+		result = await transaction.wait()        /* check for the result of the transaction */
+	})
+			it('tracks the token deposit', async () => {
+				expect(await token1.balanceOf(exchange.address)).to.equal(amount)
+				expect(await exchange.tokens(token1.address, user1.address)).to.equal(amount)
+				expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(amount)
+			})
+
+			it('emits a Deposit event', async () => {
+				const event = result.events[1] //2 events are emitted
+ 				expect(event.event).to.equal('Deposit')
+
+ 				const args = event.args
+				expect(args.token).to.equal(token1.address)
+ 				expect(args.user).to.equal(user1.address)
+ 				expect(args.amount).to.equal(amount)
+ 				expect(args.balance).to.equal(amount)
+
+			})
+		})
+		
+		describe('Failure', () => {
+			it('fails when no tokens are approved', async () => {
+				//don't approve any tokens before depositing
+				await expect(exchange.connect(user1).depositToken(token1.address, amount)).to.be.reverted
+			})
+		})
+	})
+
 })
